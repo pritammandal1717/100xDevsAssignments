f121b64ff9789c9e863f1d686632fb140154810b
Pritam Mandal <pritammandal1717@gmail.com>
2024-12-02 08:07:07 +0530

    Assignment5 is done

--- Week-7/src/components/Assignment5.jsx
+++ Week-7/src/components/Assignment5.jsx
@@ -3,63 +3,128 @@ import axios from 'axios'
 import { FaGithub } from "react-icons/fa";
 
 function Assignment5() {
+    const [input, setInput] = useState("");
     const [gitUser, setGitUser] = useState({
         name: "",
         avatar: "",
         bio: "",
         noOfRepos: "",
-        repos: [],
         githubUrl: "",
         noOfFollowers: "",
         noOfFollowings: "",
         memberSince: "",
         portfolio: "",
-        followers: [],
-        followings: [],
     })
-    console.log(gitUser);
+
+    async function downloadData(username) {
+        const response = await axios.get(`https://api.github.com/users/${username}`)
+        const data = response.data;
+
+        setGitUser({
+            name: data.name,
+            avatar: data.avatar_url,
+            bio: data.bio,
+            noOfRepos: data.public_repos,
+            githubUrl: data.html_url,
+            noOfFollowers: data.followers,
+            noOfFollowings: data.following,
+            memberSince: data.created_at,
+            portfolio: data.blog,
+        })
+    }
 
 
-    useEffect(() => {
-        (async () => {
-            // const response = await axios.get()
-            const data = response.data;
-            console.log(data);
-            setGitUser({
-                name: data.name,
-                avatar: data.avatar_url,
-                bio: data.bio,
-                noOfRepos: data.public_repos,
-                repos: data.repos_url,
-                githubUrl: data.html_url,
-                noOfFollowers: data.followers,
-                noOfFollowings: data.following,
-                memberSince: data.created_at,
-                portfolio: data.blog,
-                followers: data.followers_url,
-                followings: data.following_url,
-            })
-        })()
-    }, [])
-    return (
-        <div className="w-screen h-screen flex flex-col justify-center items-center">
-            <div className="w-2/4 flex flex-row justify-center items-center text-3xl text-slate-900">
-                <FaGithub className="mr-4" /><p>GitHub Profile Card</p>
-            </div>
-            <div className="flex flex-col bg-slate-700 rounded-md mt-5 p-5">
-                <div className="flex flex-row justify-between bg-slate-700">
-                    <div className="border-4 border-amber-400 rounded-full">
-                        <img className="rounded-full " src="profile.jpeg" alt="" />
-                    </div>
-                    <div className="mt-5 ml-10">
-                        <p className="text-slate-200 text-3xl">Your Name</p>
-                        <p className="text-slate-200 text-xl">your id</p>
+const memberSince = new Date(gitUser.memberSince);
+
+const calculateDuration = () => {
+    const now = new Date();
+    const diff = now - memberSince;
+
+    const seconds = Math.floor(diff / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+    const months = Math.floor(days / 30.44);
+    const years = Math.floor(months / 12);
+
+    const remainingMonths = months % 12;
+    const remainingDays = days % 30.44;
+    const remainingHours = hours % 24;
+    const remainingMinutes = minutes % 60;
+    const remainingSeconds = seconds % 60;
+
+    return {
+        years,
+        months: remainingMonths,
+        days: Math.floor(remainingDays),
+        hours: remainingHours,
+        minutes: remainingMinutes,
+        seconds: remainingSeconds,
+    };
+};
+
+const [duration, setDuration] = useState(calculateDuration());
+
+useEffect(() => {
+
+    if (gitUser.memberSince != "") {
+        const interval = setInterval(() => {
+            setDuration(calculateDuration());
+        }, 1000);
+        return () => clearInterval(interval);
+    }
+
+}, [gitUser])
+return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center bg-slate-300">
+
+        <div className="w-2/4 flex flex-row justify-center items-center text-3xl text-slate-900 font-mono font-medium">
+            <FaGithub className="mr-4" /><p>GitHub Profile Card</p>
+        </div>
+        <div className="w-[50%] flex flex-row justify-center mt-5">
+            <input className="w-[50%] h-10 rounded-md bg-black border p-2 font-mono text-cyan-200 mr-5" type="text" placeholder="Enter Your Github Username, e.g. john123" onInput={(e) => setInput(e.target.value)} />
+            <button className="px-3 py-1 bg-cyan-300 rounded-md font-mono font-semibold" onClick={() => downloadData(input)}>Get Profile Card</button>
+        </div>
+        {gitUser.name ?
+            <div className="card-wrapper bg-cyan-600 rounded-md m-5 ">
+                <div className="card-content">
+                    <div className="flex flex-col rounded-md m-1 p-5">
+                        <div className=" flex justify-end">
+                            <button className="px-2 py-1 bg-cyan-300 rounded-md font-mono tracking-tight text-slate-800" onClick={(e) => {
+                                window.open(gitUser.githubUrl, "_blank")
+
+                            }}>Visit My Profile</button>
+                        </div>
+                        <div className="flex flex-row justify-between">
+                            <div className="w-100">
+                                <img className="rounded-full border-4 border-violet-300" width={250} src={gitUser.avatar} alt="" />
+                            </div>
+                            <div className="mt-5 ml-10">
+                                <p className="font-mono text-violet-200 text-4xl">{gitUser.name}</p>
+                                <p className="font-mono text-cyan-100 text-lg">{gitUser.bio}</p>
+                                <div className="my-3 border-t-2 border-cyan-600"></div>
+                                <div className="flex flex-row">
+                                    <p className="mr-5 font-mono text-violet-300 text-lg">Repos: {gitUser.noOfRepos}</p>
+                                    <p className="mr-5 font-mono text-violet-300 text-lg">Followers: {gitUser.noOfFollowers}</p>
+                                    <p className="font-mono text-violet-300 text-lg">Following: {gitUser.noOfFollowings}</p>
+                                </div>
+                                <div className="flex flex-row pr-2">
+                                    <p className="mr-2 font-mono text-fuchsia-400 text-lg">Member Since : </p>
+                                    <p className="font-mono text-teal-400 text-lg">{duration.years} years {duration.months} months {duration.days} days - {duration.hours} hours {duration.minutes} minutes {duration.seconds} seconds</p>
+                                </div>
+                                {gitUser.portfolio ?
+                                    <div className="mt-5 flex justify-center">
+                                        <button className="bg-violet-300 font-mono px-3 py-1 rounded-md" onClick={(e) => { window.open(gitUser.portfolio, "_blank") }}>Portfolio</button>
+                                    </div>
+                                    : null}
+                            </div>
+                        </div>
                     </div>
                 </div>
-
             </div>
-        </div>
-    )
+            : null}
+    </div>
+)
 }
 
 export default Assignment5
